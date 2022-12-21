6442f4eac58cd776cce9cc237f3062906e027683
Bazhen <bazhen.paseka@gmail.com>
2022-12-21 10:07:31 +0200

    i2c not work

---
+++ Drivers/Local_config/lcd1602_local_config.h
@@ -0,0 +1,15 @@
+/*
+ * lcd1602_local_config.h
+ *
+ *  Created on: 4 жовт. 2022 р.
+ *      Author: paseka
+ */
+
+#ifndef LCD1602_FC113_SM_LCD1602_LOCAL_CONFIG_H_
+#define LCD1602_FC113_SM_LCD1602_LOCAL_CONFIG_H_
+
+	//#define 	ADR_I2C_FC113 0x27
+	#define 	ADR_I2C_FC113 0x3F
+	#define		LCD1602
+
+#endif /* LCD1602_FC113_SM_LCD1602_LOCAL_CONFIG_H_ */
